046884518b7da1bca000e2cc1551d3666b81b4a8
Nuogz <zheung@vip.qq.com>
2022-04-07 16:18:56 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,38 @@
+import './lib/init.js';
+
+import StaffWock from '@hey/hey-staff-wock';
+
+import PKG from './lib/global/package.js';
+import C from './lib/global/config.js';
+import G from './lib/global/log.js';
+
+import staff from './src/staff.js';
+
+
+const wockStaff = new StaffWock(
+	C.target,
+	PKG.name, C.auth.id,
+	C.auth.who, C.auth.token,
+	C.push.interval, staff,
+	(...params) => G.info('Wock', '信息', ...params),
+	(...params) => G.error('Wock', '错误', ...params),
+);
+
+
+wockStaff.add('auth-successful', () => G.info('~[Hey]通讯', '认证', '✔ '));
+wockStaff.add('auth-failed', error => G.error('~[Hey]通讯', '认证', `✖ ${error.message ?? error}`));
+wockStaff.add('staff-start', () => G.info('~[Hey]通讯', '开始~[工作]', '✔ '));
+wockStaff.add('staff-stop', () => G.info('~[Hey]通讯', '停止~[工作]', '✔ '));
+
+wockStaff.add('set-auth', (id, who, token) => C.$.edit('auth', auth => {
+	G.info('~[Hey]通讯', '更新~[认证信息]', '✔ ',
+		`~[id]~{${id}}`,
+		`~[who]~{${who}}`,
+		`~[token]~{${token}}`,
+	);
+
+	return { id, who, token };
+}));
+
+
+wockStaff.open();
---
+++ src/staff.js
@@ -0,0 +1,53 @@
+import Axios from 'axios';
+import HttpsProxyAgent from 'https-proxy-agent';
+
+import C from '../lib/global/config.js';
+import G from '../lib/global/log.js';
+
+
+const proxy = C.proxy ? new HttpsProxyAgent(C.proxy) : null;
+
+
+if(!C.release) { C.$.edit('release', () => ({})); }
+
+
+const staff = async hey => {
+	for(const repo in C.push.repo) {
+		if(!C.release[repo]) { C.$.edit('release', packagesAll => (packagesAll[repo] = []) && void 0); }
+
+		const channel = C.push.repo[repo];
+		try {
+			const url = `https://api.github.com/repos/${repo}/releases`;
+
+			const { data: releases } = await Axios.get(url, { responseType: 'json', httpsAgent: proxy });
+
+			const release = releases.sort((a, b) => b.id - a.id).find(rel => channel == 'pre' ? true : !rel.prerelease);
+			const tag = release.tag_name ?? release.name;
+
+
+			if(!C.release[repo].includes(release.tag_name)) {
+				C.$.edit('release', packagesAll => packagesAll[repo].unshift(tag) && void 0);
+
+
+				G.info('士大夫', `~[${repo}]`, `✔ 发现新~[发布]~{${tag}}`);
+
+
+				hey({
+					title: `${repo} 有新发布啦！`,
+					body: release.name,
+					data: `https://github.com/${repo}/releases/tag/${tag}`,
+					tag: `${repo}`
+				});
+			}
+			else {
+				G.info('士大夫', `~[${repo}]`, `○ 暂无新~[发布]`);
+			}
+		}
+		catch(error) {
+			G.error('士大夫', `~[${repo}]`, `✖ ${error?.message ?? error}`);
+		}
+	}
+};
+
+
+export default staff;
